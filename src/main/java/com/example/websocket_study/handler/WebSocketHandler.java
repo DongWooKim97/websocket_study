6bfe5d798eca6f9c581dbeab4ed8dce9a08199ce
김동우 <nanaukim@naver.com>
2023-07-20 15:45:48 +0900

    feat: 웹소켓 핸들러 파일 생성

---
+++ src/main/java/com/example/websocket_study/handler/WebSocketHandler.java
@@ -0,0 +1,31 @@
+package com.example.websocket_study.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        String payload = message.getPayload();
+        log.info("payload : {}", payload);
+
+        session.sendMessage(new TextMessage(payload));
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        TextMessage initialGreeting = new TextMessage("Welcome to bxxloob's velog!! :)");
+        session.sendMessage(initialGreeting);
+    }
+
+}
